a2c9155242a83cb3fa9922a0c5658086435b3545
ian-bravo <bravo.ian@gmail.com>
2023-11-06 14:11:35 -0800

    write and pass test for coinCounter function Co-authored-by: Moshe Atia <moshikoatia@gmail.com>

--- __tests__/coinCounter.test.js
+++ __tests__/coinCounter.test.js
@@ -1,6 +1,9 @@
 import coinCounter from "../src/js/CoinCounter";
 
 describe('coinCounter', () => {
-  
+  test('should verify data type', () => {
+    const Q = 0.25
+    expect(typeof Q).toBe('number');
+  })
 
 });
