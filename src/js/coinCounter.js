32d9591730794aeb0b0a34acefb0ba5307a835ea
ian-bravo <bravo.ian@gmail.com>
2023-11-06 16:22:21 -0800

    EoD
    Co-authored-by: Moshe Atia <moshikoatia@gmail.com>

--- src/js/coinCounter.js
+++ src/js/coinCounter.js
@@ -9,16 +9,19 @@ export default class coinCounter {
 function breakMoney(userInput) {
   const listOfCoins = [25, 10, 5, 1];
   let newList = [];
-
-  for (let i=0; i<=3; i++)
+  for (let i=0; i<4; i++)
   {
-    
     coinsAmount = Math.floor(userInput / listOfCoins[i]);
     userInput %= listOfCoins[i];
     newList[i] = coinsAmount;
-    
   }
   return newList;
 }
 
-breakMoney();
+const listOfCoins = [25, 10, 5, 1];
+const newList = listOfCoins.map(function(userInput){
+  coinsAmount = Math.floor(userInput / listOfCoins);
+  userInput %= listOfCoins;
+  newList = coinsAmount;
+  return newList;
+})
